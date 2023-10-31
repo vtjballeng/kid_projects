28012b9f4fdbba8e6853726564555f86425fbef8
James Ballenger <vtjballeng@gmail.com>
2023-10-30 21:19:08 -0400

    Make the arc reactor spin on 28 lights

--- Code/Ironman_Arc_Reactor/Ironman_Arc_Reactor.ino
+++ Code/Ironman_Arc_Reactor/Ironman_Arc_Reactor.ino
@@ -12,6 +12,7 @@
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 28 // Popular NeoPixel ring size
+#define GROUPSIZE 7 // How many LEDS to light togther in a circular sweeping pattern
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
@@ -19,15 +20,17 @@
 // strandtest example for more information on possible values.
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVALFOR 100 // Time (in milliseconds) to pause between pixels
-#define DELAYVALMAIN 1000 // Time (in milliseconds) to pause between pixels
+#define DELAYVALFOR 15 // Time (in milliseconds) to pause between pixels
+#define DELAYVALMAIN 15 // Time (in milliseconds) to pause between pixels
 
 void setup() {
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  //pixels.clear(); // Set all pixel colors to 'off'
+  //pixels.show();   // Send the updated pixel colors to the hardware.
+  //delay(DELAYVALMAIN); // Pause before next pass through main void loop
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
@@ -35,11 +38,18 @@ void loop() {
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
+    int j=i-GROUPSIZE;
+    int k=i+(NUMPIXELS-GROUPSIZE);
     pixels.setPixelColor(i, pixels.Color(150, 150, 150));
-
+    pixels.setPixelColor(j, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(k, pixels.Color(0, 0, 0));
+  //  pixels.setPixelColor(k, pixels.Color(150, 150, 150));
+  //  pixels.setPixelColor(k-1, pixels.Color(0, 0, 0));
+ 
     pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVALFOR); // Pause before next pass through for loop
+    delay(DELAYVALFOR); // Pause before next pass through for loop    
   }
-  delay(DELAYVALMAIN); // Pause before next pass through main void loop
+ // pixels.setPixelColor(26, pixels.Color(0, 0, 0));
+ // pixels.setPixelColor(27, pixels.Color(0, 0, 0));
+  //delay(DELAYVALMAIN); // Pause before next pass through main void loop
 }
